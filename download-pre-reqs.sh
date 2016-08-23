0fe9fb90ffb05c5865e863d12a9e16d86849a598
Lucas Russo <lerwys@gmail.com>
2016-08-23 18:24:11 -0300

    download-pre-reqs.sh: clone and build naming application

--- download-pre-reqs.sh
+++ download-pre-reqs.sh
@@ -4,3 +4,18 @@
 . ./env-vars.sh
 
 curl -o deploy/postgresql-${POSTGRES_VERSION}.jar https://jdbc.postgresql.org/download/postgresql-${POSTGRES_VERSION}.jar
+
+# Clone ESS java config
+git clone --branch=${ESS_JAVA_CONFIG_VERSION} https://bitbucket.org/europeanspallationsource/ess-java-config
+# Clone naming-convention-tool
+git clone --branch=${NAMING_CONVENTION_VERSION} https://github.com/lerwys/naming-convention-tool
+
+# Build Java Config
+cd ess-java-config
+mvn install
+cd ..
+
+# Build Naming Convention tool
+cd naming-convention-tool
+mvn install -P local
+cd ..
