04cb3b0aa0cdf9ae2e4f3077880fd5447dbcd290
Lucas Russo <lerwys@gmail.com>
2016-08-23 17:16:31 -0300

    run-wildfly.sh: fix wrong comment

--- run-wildfly.sh
+++ run-wildfly.sh
@@ -11,7 +11,7 @@ LOCAL_WILDFLY_PORT="$4"
 ADMIN_PORT="$5"
 LOCAL_ADMIN_PORT="$6"
 
-# Run postgres
-docker run  --name naming-service-wildfly --net ${NET_NAME} --dns ${DNS_IP} \
+# Run Wildfly
+docker run --name naming-service-wildfly --net ${NET_NAME} --dns ${DNS_IP} \
     -p ${LOCAL_WILDFLY_PORT}:${WILDFLY_PORT} -p ${LOCAL_ADMIN_PORT}:${ADMIN_PORT}\
     -d lerwys/docker-naming-service-wildfly /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
