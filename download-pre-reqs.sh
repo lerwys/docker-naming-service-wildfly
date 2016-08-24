100f7f6292512aabdc3fc4be1f0bed394db9e939
Lucas Russo <lerwys@gmail.com>
2016-08-24 14:44:30 -0300

    download-pre-reqs.sh,env-vars.sh: download wait-for-it tool

--- download-pre-reqs.sh
+++ download-pre-reqs.sh
@@ -9,6 +9,8 @@ curl -o deploy/postgresql-${POSTGRES_VERSION}.jar https://jdbc.postgresql.org/do
 git clone --branch=${ESS_JAVA_CONFIG_VERSION} https://bitbucket.org/europeanspallationsource/ess-java-config ${ESS_JAVA_CONFIG_REPO}
 # Clone naming-convention-tool
 git clone --branch=${NAMING_CONVENTION_VERSION} https://github.com/lerwys/naming-convention-tool ${NAMING_CONVENTION_REPO}
+# Clone wait-for-it
+git clone --branch=${WAIT_FOR_IT_VERSION} https://github.com/vishnubob/wait-for-it scripts/${WAIT_FOR_IT_REPO}
 
 # Build Java Config
 cd ${ESS_JAVA_CONFIG_REPO}
