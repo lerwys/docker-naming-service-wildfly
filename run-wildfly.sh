16ba5af13145519e290b9645a2b1bb6368246614
Lucas Russo <lerwys@gmail.com>
2016-12-01 09:45:51 -0200

    run-wildfly.sh: fix wrong entrypoint

--- run-wildfly.sh
+++ run-wildfly.sh
@@ -9,6 +9,6 @@ set -u
 # Run Wildfly
 docker run --name ${NAMING_CONVENTION_DOCKER_RUN_NAME} --net ${NET_NAME} --dns ${DNS_IP} \
     -p ${LOCAL_WILDFLY_PORT}:${WILDFLY_PORT} -p ${LOCAL_ADMIN_PORT}:${ADMIN_PORT} \
-    -d --entrypoint "/docker-entrypoint.sh" ${NAMING_CONVENTION_DOCKER_ORG_NAME}/${NAMING_CONVENTION_DOCKER_IMAGE_NAME} \
+    -d --entrypoint "/wait-for-it.sh" ${NAMING_CONVENTION_DOCKER_ORG_NAME}/${NAMING_CONVENTION_DOCKER_IMAGE_NAME} \
     ${DB_NAME}:${DB_PORT} -- /opt/jboss/wildfly/bin/standalone.sh \
     -b 0.0.0.0 -bmanagement 0.0.0.0
